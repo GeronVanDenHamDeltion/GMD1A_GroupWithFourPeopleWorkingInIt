6641196b12ff9653ed5a42796de7b41e6aa4e0af
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-05-18 14:39:55 +0200

    LeliDunno

---
+++ Source/Project4/Assets/Models/Hand/HandScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandScript : MonoBehaviour
+{
+    public GameObject hand;
+    public Animator anim;
+
+    void Start()
+    {
+        hand = this.gameObject;
+        anim = hand.GetComponent<Animator>();
+    }
+
+}
