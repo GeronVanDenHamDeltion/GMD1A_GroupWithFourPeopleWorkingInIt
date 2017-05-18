4fa73b87230dcc60c18cf19bfcc26fbaa17df4d6
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-05-18 15:43:28 +0200

    Raycast hand

--- Source/Project4/Assets/Models/Hand/HandScript.cs
+++ Source/Project4/Assets/Models/Hand/HandScript.cs
@@ -5,12 +5,42 @@ using UnityEngine;
 public class HandScript : MonoBehaviour
 {
     public GameObject hand;
+    public GameObject player;
     public Animator anim;
+
+    
+    public float sensorLength;
+
 
     void Start()
     {
+        player = GameObject.Find("Player");
         hand = this.gameObject;
         anim = hand.GetComponent<Animator>();
     }
+
+    void FixedUpdate()
+    {
+        CheckForObject();
+    }
+
+    public void CheckForObject()
+    {
+        RaycastHit hit;
+        Transform sensorStarPos = GameObject.Find("Main Camera").transform;
+
+        Debug.DrawRay(sensorStarPos.position, sensorStarPos.forward * 100);
+        if (Physics.Raycast(sensorStarPos.position, sensorStarPos.transform.forward, out hit, sensorLength))
+        {
+            if (hit.collider.CompareTag("Terrain") == false)
+            {
+                if (hit.collider.CompareTag("PickUpCollect") == true)
+                {
+                    print("pickup");
+                }
+            }
+        }
+    }
+
 
 }
