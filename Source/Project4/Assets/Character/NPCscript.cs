2a6b2dc8c93e3eeaedcb8f44fa0e4f71ef1ce795
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-05-31 18:46:21 +0200

    HumanNPC ready for AI

---
+++ Source/Project4/Assets/Character/NPCscript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCscript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public Animator anim;
+
+    public enum AnimationState { Idle, Walk, Run };
+    public AnimationState animationState;
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if ( animationState == AnimationState.Idle )
+        {
+            anim.SetBool("bNPCidle", true);
+            anim.SetBool("bNPCwalk", false);
+            anim.SetBool("bNPCrun", false);
+        }
+
+        if (animationState == AnimationState.Walk)
+        {
+            anim.SetBool("bNPCwalk", true);
+            anim.SetBool("bNPCidle", false);
+            anim.SetBool("bNPCrun", false);
+        }
+
+        if (animationState == AnimationState.Run)
+        {
+            anim.SetBool("bNPCrun", true);
+            anim.SetBool("bNPCidle", false);
+            anim.SetBool("bNPCwalk", false);
+        }      
+    }
+}
