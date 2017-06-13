3b319193b2a857f5cef441538f29c85c02f1b3e8
Geron van den Ham <geron.ham@xs4all.nl>
2017-06-13 10:00:39 +0200

    hi

---
+++ Source/Project4/Assets/BookManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookManager : MonoBehaviour
+{
+    public Image pageOne;
+    public Image pageTwo;
+    public Image pageThree;
+    public Image pageFourNoteOne;
+    public Image pageFive;
+    public Image pageSixNoteTwo;
+    public Image pageSeven;
+    public Image pageEightNoteThree;
+    public bool noteOnePickupUp;
+    public bool noteTwoPickupUp;
+    public bool noteThreePickupUp;
+
+    public void enableNote(int i)
+    {
+
+    }
+}
---
+++ Source/Project4/Assets/Inventory/Note.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public int noteNumber;
+    public BookManager bookMan;
+    void onTriggerEnter(Collider collider)
+    {
+        bookMan.enableNote(noteNumber);
+    }
+}
