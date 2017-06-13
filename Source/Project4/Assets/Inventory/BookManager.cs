c44c70b9a178b9bc3759499224230c01427348c7
Geron van den Ham <geron.ham@xs4all.nl>
2017-06-13 11:14:26 +0200

    testje

--- Source/Project4/Assets/Inventory/BookManager.cs
+++ Source/Project4/Assets/Inventory/BookManager.cs
@@ -5,6 +5,14 @@ using UnityEngine.UI;
 
 public class BookManager : MonoBehaviour
 {
+    public enum pages
+    {
+        first,
+        second,
+        third,
+        Fourth
+    }
+    public pages currentPageNumer;
     public Image pageOne;
     public Image pageTwo;
     public Image pageThree;
@@ -13,7 +21,39 @@ public class BookManager : MonoBehaviour
     public Image pageSixNoteTwo;
     public Image pageSeven;
     public Image pageEightNoteThree;
+    public Image rippedOutPage;
     public bool noteOnePickupUp;
     public bool noteTwoPickupUp;
     public bool noteThreePickupUp;
+
+    public void enableNote(int i)
+    {
+        if (i == 1)
+        {
+            noteOnePickupUp = true;
+        }
+        if (i == 2)
+        {
+            noteTwoPickupUp = true;
+        }
+        if (i == 3)
+        {
+            noteThreePickupUp = true;
+        }
+    }
+    public void nextPage()
+    {
+        if (currentPageNumer == pages.first)
+        {
+
+        }
+    }
+    public void previousPage()
+    {
+        if (currentPageNumer == pages.first)
+        {
+            return;
+        }
+    }
+
 }
