e5af3cb388b055c0242342af58509c8047d53857
Geron van den Ham <geron.ham@xs4all.nl>
2017-06-19 10:28:37 +0200

    save and load

--- Source/Project4/Assets/Menu/DataHolder.cs
+++ Source/Project4/Assets/Menu/DataHolder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DataHolder
 {
     public Vector3 playerlocation;
+    public Vector3 playerRotation;
     public List<bool> pageAbleToSee = new List<bool>();
     public int currentScene;
 }
